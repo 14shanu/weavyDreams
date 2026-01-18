b1ce17b6ae2f39f8131f516589f22a3371fbc934
abhisheksinghal1497 <abhishek.s.singhal@pwc.com>
2026-01-18 21:31:49 +0530

    updated logger

--- src/lib/utils/logger.ts
+++ src/lib/utils/logger.ts
@@ -1,5 +1,10 @@
+// export function logError(error: unknown, context?: string) {
+//   if (process.env.NODE_ENV !== "production") {
+//     console.error("[Error]" + (context ? \` \${context}\` : ""), error);
+//   }
+// }
 export function logError(error: unknown, context?: string) {
   if (process.env.NODE_ENV !== "production") {
-    console.error("[Error]" + (context ? \` \${context}\` : ""), error);
+    console.error("[Error]" + (context ? ` ${context}` : ""), error);
   }
 }
